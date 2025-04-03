ae80c7b9e850af8a5c768ed947ec72fcac0dd9b5
wama <36957874+april1026@users.noreply.github.com>
2025-04-03 22:37:57 +0800

    feat: add urls & views

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
-# Create your views here.
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the index page.")
+
+
+def about(request):
+    return HttpResponse("This is the about page.")
+
+
+def contact(request):
+    return HttpResponse("This is the contact page.")
